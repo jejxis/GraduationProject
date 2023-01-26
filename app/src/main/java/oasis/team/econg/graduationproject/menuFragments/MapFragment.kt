a3972f83aa301a6bc45d03580837ee9f2f8fa2c7
jejxis <3190021@gmail.com>
2023-01-27 02:55:20 +0900

    MapFragment.kt.. to do - show now where

--- app/src/main/java/oasis/team/econg/graduationproject/menuFragments/MapFragment.kt
+++ app/src/main/java/oasis/team/econg/graduationproject/menuFragments/MapFragment.kt
@@ -1,15 +1,26 @@
 package oasis.team.econg.graduationproject.menuFragments
 
-import android.R
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -18,9 +29,20 @@ import oasis.team.econg.graduationproject.databinding.FragmentMapBinding
 
 
 class MapFragment : Fragment(), OnMapReadyCallback {
-    private lateinit var googleMap: GoogleMap
+    private lateinit var mMap: GoogleMap
     lateinit var binding: FragmentMapBinding
     lateinit var main: MainActivity
+
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+    lateinit var mLastLocation: Location
+    private lateinit var mLocationRequest: com.google.android.gms.location.LocationRequest
+    private val REQUEST_PERMISSION_LOCATION = 10
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            mLastLocation = locationResult.lastLocation!!
+        }
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -33,10 +55,76 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentMapBinding.inflate(inflater, container, false)
-        binding.map.onCreate(savedInstanceState)
         binding.map.getMapAsync(this)
+        mLocationRequest = com.google.android.gms.location.LocationRequest.create().apply {
+            priority = PRIORITY_HIGH_ACCURACY
+        }
+
+        if (checkPermissionForLocation(main)) {
+            startLocationUpdates()
+            changeLocation()
+        }
 
         return binding.root
+    }
+
+    private fun changeLocation() {
+        Log.d("MY", "changeLocation: call")
+        val NOW = LatLng(mLastLocation.latitude, mLastLocation.longitude)
+        Log.d("MY", "changeLocation: after NOW")
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(NOW)
+        markerOptions.title("현재 위치")
+        markerOptions.snippet("현재 위치입니다.")
+        mMap.addMarker(markerOptions)
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NOW, 10.0f))
+
+        Toast.makeText(main, "${NOW.latitude}, ${NOW.longitude}", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startLocationUpdates() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(main)
+        if (ActivityCompat.checkSelfPermission(main, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(main,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    /*fun onLocationChanged(location: Location) {
+        mLastLocation = location
+    }*/
+
+    private fun checkPermissionForLocation(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                true
+            } else {
+                ActivityCompat.requestPermissions(main, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION)
+                false
+            }
+        } else {
+            true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+
+            } else {
+                Toast.makeText(main, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -44,19 +132,8 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         binding.map.onSaveInstanceState(outState)
     }
 
-    override fun onMapReady(p0: GoogleMap) {
-        googleMap = p0
-
-        val SEOUL = LatLng(37.56, 126.97)
-
-        val markerOptions = MarkerOptions()
-        markerOptions.position(SEOUL)
-        markerOptions.title("서울")
-        markerOptions.snippet("한국의 수도")
-        googleMap.addMarker(markerOptions)
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL))
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13.0f))
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 
     override fun onStart() {
