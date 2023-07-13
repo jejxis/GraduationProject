aa31fca7e2b2e829afcbdb35dddd2a05f530ee19
jejxis <69408166+jejxis@users.noreply.github.com>
2023-07-13 18:30:09 +0900

    Update IRetrofit.kt
    deleted url

--- app/src/main/java/oasis/team/econg/graduationproject/retrofit/IRetrofit.kt
+++ app/src/main/java/oasis/team/econg/graduationproject/retrofit/IRetrofit.kt
@@ -11,97 +11,97 @@ import retrofit2.http.*
 interface IRetrofit {
 
     //유저 로그인
-    @POST("/api/signin")
+    @POST("/~/~")
     fun signIn(@Body param: LoginDto): Call<JsonElement>
 
     //회원가입
     @Multipart
     @JvmSuppressWildcards
-    @POST("/api/signup")
+    @POST("/~/~")
     fun signUp(@Part("key") key: RequestBody, @Part file: MultipartBody.Part?): Call<JsonElement>
 
     //내 식물 목록 불러오기
-    @GET("/api/plants")
+    @GET("/~/~")
     fun getPlants(@Header("Authorization") auth: String): Call<JsonElement>
 
     //식물 등록하기
     @Multipart
-    @POST("/api/plants")
+    @POST("/~/~")
     fun postPlants(@Header("Authorization") auth: String, @Part("key") key: RequestBody, @Part file: MultipartBody.Part?): Call<JsonElement>
 
     //다이어리 리스트 가져오기
-    @GET("/api/journals/{plantId}")
+    @GET("/~/~/{plantId}")
     fun getJournals(@Header("Authorization") auth: String, @Path("plantId") plantId: Long): Call<JsonElement>
 
     //다이어리 올리기
     @Multipart
-    @POST("/api/journals/{plantId}")
+    @POST("/~/~/{plantId}")
     fun postJournals(@Header("Authorization") auth: String, @Path("plantId") plantId: Long,
     @Part("key") key: RequestBody, @Part file: MultipartBody.Part?): Call<JsonElement>
 
     //식물샵 가져오기
-    @GET("/api/places")
+    @GET("/~/~")
     fun getPlaces(@Header("Authorization") auth: String,
                   @Query("x") x: String, @Query("y") y: String): Call<JsonElement>
 
     //날씨 가져오기
-    @GET("/api/home")
+    @GET("/~/~")
     fun getWeather(@Header("Authorization") auth: String,
                    @Query("x") x: String, @Query("y") y: String): Call<JsonElement>
 
     //사용자 정보 가져오기
-    @GET("/api/user")
+    @GET("/~/~")
     fun getUser(@Header("Authorization") auth: String): Call<JsonElement>
 
     //사용자 정보(프로필, 닉네임 수정)
     @Multipart
     @JvmSuppressWildcards
-    @PATCH("/api/user/update")
+    @PATCH("/~/~/~")
     fun updateUserInfo(@Header("Authorization") auth: String, @Part("name") name: RequestBody, @Part("change") change: RequestBody, @Part file: MultipartBody.Part?): Call<JsonElement>
 
     //비밀번호 변경
-    @PATCH("/api/user/changepw")
+    @PATCH("/~/~/~")
     fun changeUserPw(@Header("Authorization") auth: String, @Body param: PwDto): Call<JsonElement>
 
     //전체 식물도감
-    @GET("/api/garden")
+    @GET("/~/~")
     fun getGarden(@Header("Authorization") auth: String): Call<JsonElement>
 
     //식물도감 디테일
-    @GET("/api/garden/{gardenId}")
+    @GET("/~/~/{gardenId}")
     fun getGardenDetail(@Header("Authorization") auth: String, @Path("gardenId") gardenId: Long): Call<JsonElement>
 
     //식물도감 검색
-    @GET("/api/garden/search")
+    @GET("/~/~/~")
     fun searchGarden(@Header("Authorization") auth: String, @Query("keyword") keyword: String): Call<JsonElement>
 
     //찜한 식물 가져오기
-    @GET("/api/bookmarks")
+    @GET("/~/~")
     fun getBookmarks(@Header("Authorization") auth: String): Call<JsonElement>
 
     //식물 찜하기
-    @POST("/api/bookmarks/{gardenId}")
+    @POST("/~/~/{gardenId}")
     fun postBookmarks(@Header("Authorization") auth: String, @Path("gardenId") gardenId: Long): Call<JsonElement>
 
     //물주기, 분갈이, 영양제 주기 표시
-    @POST("/api/calendars/{plantId}")
+    @POST("/~/~/{plantId}")
     fun postCalendars(@Header("Authorization") auth: String, @Path("plantId") plantId: Long, @Query("type") type: String): Call<JsonElement>
 
     //물주기, 분갈이, 영양제 준 날들 가져오기
-    @GET("/api/calendars")
+    @GET("/~/~")
     fun getCalendars(@Header("Authorization") auth: String):Call<JsonElement>
 
     //다이어리 삭제
-    @DELETE("/api/journals/{journalId}")
+    @DELETE("/~/~/{journalId}")
     fun deleteJournals(@Header("Authorization") auth: String, @Path("journalId") journalId: Long): Call<JsonElement>
 
     //사용자가 입력한 특정 식물 정보 받아오기
-    @GET("/api/plants/{plantId}")
+    @GET("/~/~/{plantId}")
     fun getPlantInfo(@Header("Authorization") auth: String, @Path("plantId") plantId: Long): Call<JsonElement>
 
-    @DELETE("/api/plants/{plantId}")
+    @DELETE("/~/~/{plantId}")
     fun deletePlants(@Header("Authorization") auth: String, @Path("plantId") plantId: Long): Call<JsonElement>
 
-    @PATCH("/api/plants/star/{plantId}")
+    @PATCH("/~/~/~/{plantId}")
     fun starPlants(@Header("Authorization") auth: String, @Path("plantId") plantId: Long): Call<JsonElement>
 }
